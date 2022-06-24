3e8b1b688ad88c4d40c5cd23eb407deabdc7ae8e
user-nexus <henrique.p.lima0@gmail.com>
2022-06-23 21:25:56 -0300

    Create 'Add note' section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 function App() {
   return (
     <div>
-      <header>
+      <header className="nav">
         <h1>Notetly</h1>
+        <p>A note taking app</p>
       </header>
+      <main>
+        <div className="note-area">
+          <div className="note-area-add">
+            <h2>Add a new note</h2>
+
+            <div className="note-area-add_input">
+              <label>Note title</label>
+              <input type="text" />
+            </div>
+
+            <div className="note-area-add_input">
+              <label>Note content</label>
+              <textarea rows="10"/>
+            </div>
+
+            <button>Save note</button>
+          </div>
+        </div>
+      </main>
+      <footer className="footer">
+        <a href="https://github.com/henriquepaulalima" target="_blank" rel="noopener noreferrer"><p>Henrique P. Lima &copy;</p></a>
+      </footer>
     </div>
   );
 }
