6ceef4f3cfb3bd6dc43c16a41228561b1cf4450d
user-nexus <henrique.p.lima0@gmail.com>
2022-06-28 09:13:01 -0300

    Finish styling and add responsiveness

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Grid, Typography } from "@mui/material";
 import NotesList from "./components/NotesList/NotesList";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 
@@ -5,10 +6,21 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Notetly</h1>
+        <Typography
+          textAlign={'center'} variant="h3" component="div" sx={{ m: 0, color: '#000', fontSize: 32, fontWeight: 700 }} gutterBottom>
+          Notetly
+        </Typography>
       </header>
-      <RegistrationForm />
-      <NotesList />
+      <main>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4}>
+            <RegistrationForm />
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <NotesList />
+          </Grid>
+        </Grid>
+      </main>
     </div>
   );
 }
--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -2,11 +2,6 @@ import { Button, Card, CardActions, CardContent, Typography } from '@mui/materia
 import React, { Component } from 'react';
 import './NotesList.css';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { borderColor } from '@mui/system';
-
-const theme = {
-  spacing: 16,
-}
 
 class NotesList extends Component {
   state = {}
@@ -14,15 +9,15 @@ class NotesList extends Component {
     return (
       <section>
         <Card sx={{ p: 2, m: 2, width: 300, backgroundColor: '#212121' }}>
-          <CardActions>
-            <Button variant="outlined" color='white'>Card details</Button>
-            <DeleteIcon sx={{color: '#fff'}}/>
+          <CardActions className='card-actions'>
+            <Button className='card-actions_btn' variant="contained" sx={{ color: '#000', background: '#fff' }} >Card details</Button>
+            <DeleteIcon className='card-actions_delete-btn' sx={{ color: '#fff' }} />
           </CardActions>
           <CardContent>
-            <Typography variant="h6" component="div" sx={{color: '#fff'}} gutterBottom>
+            <Typography variant="h6" component="div" sx={{ color: '#fff' }} gutterBottom>
               Title
             </Typography>
-            <Typography variant="body1" sx={{color: '#fff'}} gutterBottom>
+            <Typography variant="body1" sx={{ color: '#c0c0c0' }} gutterBottom>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus varius finibus pharetra. Morbi varius, purus nec faucibus tincidunt, dui neque volutpat metus, quis dapibus risus magna in urna.
             </Typography>
           </CardContent>
